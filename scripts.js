9544a6f033952a9061e6b4171b1b456b035840ce
ps-woramet <ps.woramet@gmail.com>
2023-03-21 08:10:07 -0700

    class overlay add toggle show at js file

--- scripts.js
+++ scripts.js
@@ -1,13 +1,18 @@
 const icon_hamburger = document.querySelector(".icon-hamburger");
 const icon_close = document.querySelector(".icon-close");
 const menu = document.querySelector(".menu");
-icon_hamburger.addEventListener("click", () => {
+const overlay = document.querySelector(".overlay");
+
+function toggle_menu(){
     icon_hamburger.classList.toggle("toggle");
     icon_close.classList.toggle("toggle");
     menu.classList.toggle("toggle");
+    overlay.classList.toggle("show");
+}
+
+icon_hamburger.addEventListener("click", () => {
+    toggle_menu();
 });
 icon_close.addEventListener("click", () => {
-    icon_hamburger.classList.toggle("toggle");
-    icon_close.classList.toggle("toggle");
-    menu.classList.toggle("toggle");
+    toggle_menu();
 });
